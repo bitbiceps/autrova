f98c92f15bfc99e7c88ce1f93650bfff31994088
Vinay Maheshwari <vinay.m@cachelabs.io>
2025-06-13 08:52:31 +0530

    ADD: Number in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,19 @@ const HeroSection = () => {
     <div className="bg-[url('https://a.travel-assets.com/flex/flexmanager/images/2019/08/19/CR_Hero.jpg')] bg-cover bg-center py-20">
       <div className="max-w-4xl mx-auto text-center">
         {/* Blurred backdrop box */}
-        <div >
-          <h1 className="text-3xl font-bold mb-4 text-white">
-            Cheap Car Rentals
-          </h1>
+        <div>
+          <div className="text-3xl font-bold mb-4 text-white">
+            <div>Cheap Car Rentals</div>
+            <a
+              href="tel:+18887685178"
+              className="inline-block text-white px-2 text-base font-medium transition bg-black/40 backdrop-blur-sm "
+            >
+              📞 +1-888-768-5178
+            </a>
+          </div>
           <p className="text-white text-sm mb-6 max-w-xl mx-auto bg-black/40 backdrop-blur-sm ">
-            Book your rental car quickly and easily with our agents. Tap below to call now.
+            Book your rental car quickly and easily with our agents. Tap below
+            to call now.
           </p>
 
           {/* Click-to-call Button */}
@@ -16,7 +23,7 @@ const HeroSection = () => {
             href="tel:+18887685178"
             className="inline-block bg-orange-600 hover:bg-orange-700 text-white px-8 py-4 rounded-full text-base font-medium transition"
           >
-            📞 Call Now
+            Call Now
           </a>
         </div>
       </div>
